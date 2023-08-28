bb4f0ed12cfbdccfa94f9c56c8f13d465d85a0fd
Sanyogita <sanyogita.yadav@vkaps.com>
2023-08-28 15:31:50 +0530

    Some changes

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -7,7 +7,6 @@ export const signUpController = async(req,res)=>{
         const hashPassword = await bcrypt.hash(password,10)
         const data = await UserModal.create({name,email,phone,password:hashPassword})
         return res.status(StatusCodes.CREATED).json({success:true, message:"Succesfull user signup", user:data})
-
     }
     catch(err){
         return res.status(StatusCodes.CREATED).json({success:false, message:"Unsuccesfull user", Error:err.message})
