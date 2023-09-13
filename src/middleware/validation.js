f9844653b1bf6548d2f70044d73a2583d02bcc08
Sanyogita <sanyogita.yadav@vkaps.com>
2023-09-13 11:37:09 +0530

    remove phone requireds

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -4,7 +4,8 @@ import { body } from 'express-validator';
 const signUpValidationRules = [
   body('name').not().isEmpty().withMessage('Name is required'),
   body('email').isEmail().withMessage('Invalid email address'),
-  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+  body('phone').optional().isMobilePhone().withMessage('Invalid phone number'),
+  body('password').optional().isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
 ];
 
 
